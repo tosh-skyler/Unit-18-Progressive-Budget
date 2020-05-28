dbeb160f47ba963595297dbe1b06bbfa78d3d821
Skyler Tosh <skylerjtosh@gmail.com>
2020-05-28 12:41:24 -0700

    Added connection to mongoDB atlas.

--- server.js
+++ server.js
@@ -1,28 +1,52 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const compression = require('compression');
 
 const PORT = 3000;
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+require('dotenv').config();
+
+
+mongoose
+	.connect(
+		`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0-3pcpg.mongodb.net/test?retryWrites=true&w=majority`,
+		{ useUnifiedTopology: true, useNewUrlParser: true }
+	)
+	.then(() => console.log('M O N G O D B   C O N N E C T E D . . .'))
+	.catch((err) => console.log('[ E R R O R ]: ' + err));
+
+// server-sent event stream
+app.get('/events', function(req, res) {
+	res.setHeader('Content-Type', 'text/event-stream');
+	res.setHeader('Cache-Control', 'no-cache');
+
+	// send a ping approx every 2 seconds
+	var timer = setInterval(function() {
+		res.write('data: ping\n\n');
+
+		// !!! this is the important part
+		res.flush();
+	}, 2000);
+
+	res.on('close', function() {
+		clearInterval(timer);
+	});
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require('./routes/api.js'));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+	console.log(`App running on port ${PORT}!`);
 });

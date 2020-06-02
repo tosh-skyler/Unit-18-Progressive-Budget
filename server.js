08b208f064076762225efb5d71fba8ee1d4fab25
Skyler Tosh <skylerjtosh@gmail.com>
2020-06-02 11:08:20 -0700

    Heroku

--- server.js
+++ server.js
@@ -2,20 +2,18 @@ const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const compression = require('compression');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
-app.use(logger('dev'));
+// Define Middleware
 
+app.use(logger('dev'));
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static('public'));
-
-require('dotenv').config();
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));

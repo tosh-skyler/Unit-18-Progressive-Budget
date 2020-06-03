decc4b82db51e6560cec2341a6632dd23c5b3bcc
Skyler Tosh <skylerjtosh@gmail.com>
2020-06-03 12:48:41 -0700

    Removed dotenv

--- server.js
+++ server.js
@@ -3,13 +3,10 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const compression = require('compression');
 
-require('dotenv').config();
-
 const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/transactions'
 
 const app = express();
-
-// Define Middleware
 
 app.use(logger('dev'));
 
@@ -19,24 +16,15 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-}
-
 mongoose
-	.connect(
-		process.env.MONGODB ||
-			'mongodb://localhost/transactions',
-		{
-			useUnifiedTopology: true,
-			useNewUrlParser: true,
-			useFindAndModify: false
-		}
-	)
+	.connect(MONGODB_URI, {
+		useUnifiedTopology: true,
+		useNewUrlParser: true,
+		useFindAndModify: false
+	})
 	.then(() => console.log('MongoDB Connected!'))
 	.catch((err) => console.log('[ Error ]: ' + err));
 
-// routes
 app.use(require('./routes/api.js'));
 
 app.listen(PORT, () => {

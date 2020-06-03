d44a217977a22dc652ce12a4168bda3f5663b7af
Skyler Tosh <skylerjtosh@gmail.com>
2020-06-03 11:33:00 -0700

    Added service worker and a webmanifest

--- server.js
+++ server.js
@@ -2,17 +2,21 @@ const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const compression = require('compression');
+
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 // Define Middleware
 
 app.use(logger('dev'));
+
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static('public'));
 
 if (process.env.NODE_ENV === 'production') {
